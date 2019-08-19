5241a86d516720de1bd1a07f9362a4c18c981e83
tamunobelemamiebaka-ogan <belema@softcom.ng>
2019-08-19 22:29:01 +0100

    Create sealed DataItem class as parent for the different data types to be displayed by RecyclerView

---
+++ app/src/main/java/com/example/android/trackmysleepquality/DataItem.kt
@@ -0,0 +1,19 @@
+package com.example.android.trackmysleepquality
+
+import com.example.android.trackmysleepquality.database.SleepNight
+
+/**
+ * Created by Belema Ogan on 2019-08-19.
+ */
+sealed class DataItem {
+
+    abstract val id: Long
+
+    data class SleepNightItem(val sleepNight: SleepNight): DataItem(){
+        override val id = sleepNight.nightId
+    }
+
+    object Header: DataItem(){
+        override val id = Long.MIN_VALUE
+    }
+}
