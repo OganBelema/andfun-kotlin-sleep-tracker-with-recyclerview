0ab14a8c6d9608398c922472186f3c9953192d90
tamunobelemamiebaka-ogan <belema@softcom.ng>
2019-08-19 16:24:51 +0100

    Update SleepNight list item using binding adapter in layout

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightItemViewHolder.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightItemViewHolder.kt
@@ -3,9 +3,6 @@ package com.example.android.trackmysleepquality.sleeptracker
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.example.android.trackmysleepquality.R
-import com.example.android.trackmysleepquality.convertDurationToFormatted
-import com.example.android.trackmysleepquality.convertNumericQualityToString
 import com.example.android.trackmysleepquality.database.SleepNight
 import com.example.android.trackmysleepquality.databinding.ListItemSleepNightBinding
 
@@ -16,21 +13,10 @@ class SleepNightItemViewHolder private constructor(
         private val listItemSleepNightBinding: ListItemSleepNightBinding):
         RecyclerView.ViewHolder(listItemSleepNightBinding.root) {
 
-    fun bind(item: SleepNight) {
-        val resource = itemView.context.resources
-        listItemSleepNightBinding.sleepLength.text = convertDurationToFormatted(item.startTimeMilli,
-                item.endTimeMilli, resource)
-        listItemSleepNightBinding.qualityString.text = convertNumericQualityToString(
-                item.sleepQuality, resource)
-        listItemSleepNightBinding.qualityImage.setImageResource(when (item.sleepQuality) {
-            0 -> R.drawable.ic_sleep_0
-            1 -> R.drawable.ic_sleep_1
-            2 -> R.drawable.ic_sleep_2
-            3 -> R.drawable.ic_sleep_3
-            4 -> R.drawable.ic_sleep_4
-            5 -> R.drawable.ic_sleep_5
-            else -> R.drawable.ic_sleep_active
-        })
+    fun bind(sleepNight: SleepNight) {
+        listItemSleepNightBinding.sleepNight = sleepNight
+        listItemSleepNightBinding.executePendingBindings()
+
     }
 
     companion object {
